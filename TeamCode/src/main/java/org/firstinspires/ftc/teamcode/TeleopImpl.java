600333c5a0610e6911df897addcd016e9be5f588
Skywalker934 <ledwards26@cathedral-elpaso.org>
2023-04-25 16:04:51 -0600

    Created teleop and anoher thing for teleop also did something with gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopImpl.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class TeleopImpl extends Teleop {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
